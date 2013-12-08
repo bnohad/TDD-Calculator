0f827c34c278460a138913ea1e4b514e2ebfc14b
bnohad <bnohad@gmail.com>
2013-12-08 11:31:58 +0200

    BLUE: SplitStringToArray() refactor

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,12 +6,19 @@ public class Calculator {
 		if(input.isEmpty())
 			return 0;
 		
-		String numbers[]=input.split(","); //split string to array of strings
+		String numbers[]=SplitStringToArray(input); //split string to array of strings
 		
 		if(numbers.length>1)
 			return (Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]));
 		
 		return Integer.parseInt(input);
+	}
+	
+	private String[] SplitStringToArray(String input)
+	{
+		String numbers[]=input.split(",");
+		
+		return numbers;
 	}
 
 }
