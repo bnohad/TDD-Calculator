34effb5d6e4db4f06d8653fd2c2f86c5a9b0b027
bnohad <bnohad@gmail.com>
2013-12-08 11:16:17 +0200

    BLUE: Tester Refactor

--- src/StringCalcTester.java
+++ src/StringCalcTester.java
@@ -1,15 +1,22 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class StringCalcTester {
+	
+	Calculator calc;
+	
+	@Before
+	public void init()
+	{
+		calc=new Calculator();
+	}
 
 	@Test
 	public void AddEmptyString_returnZero() 
 	{
-		//arrange
-		Calculator calc=new Calculator();
 		//act
 		int actual=calc.Add("");
 		//assert
@@ -19,8 +26,6 @@ public class StringCalcTester {
 	@Test
 	public void AddOneNumber_returnNumber()
 	{
-		//arrange
-		Calculator calc=new Calculator();
 		//act
 		int actual=calc.Add("1");
 		//assert
