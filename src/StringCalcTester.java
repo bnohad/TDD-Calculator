f2a2fc609ef15cd9b3b58de98c09edb9c0773c39
bnohad <bnohad@gmail.com>
2013-12-08 11:04:42 +0200

    RED: AddOneNumber_returnNumber() fails

--- src/StringCalcTester.java
+++ src/StringCalcTester.java
@@ -15,4 +15,15 @@ public class StringCalcTester {
 		//assert
 		assertEquals(0, actual);
 	}
+	
+	@Test
+	public void AddOneNumber_returnNumber()
+	{
+		//arrange
+		Calculator calc=new Calculator();
+		//act
+		int actual=calc.Add("1");
+		//assert
+		assertEquals(1, actual);
+	}
 }
